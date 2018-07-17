de18110fe236aade166ece351f6df3487002176c
scottehunt <scottehunt@gmail.com>
2018-07-17 20:06:15 +1200

    Set-up window

---
+++ Pong/src/Pong.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+	
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	private final static String WINDOW_TITLE = "Pong";
+		
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new Pong();
+	}
+
+}
