9691a5c5c54b4f28134dd48eb5af8e020a0f062f
scottehunt <scottehunt@gmail.com>
2018-07-17 20:38:17 +1200

    Added paddle

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,13 +4,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
 	
 	
@@ -31,11 +37,23 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
+	}
+	
+	private void update() {
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		
+		update();
+		repaint();
 	}
 	
 }
