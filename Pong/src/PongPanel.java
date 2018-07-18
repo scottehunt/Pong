510a8c87ed25a064fabeb251f4258a6c995f6187
scottehunt <scottehunt@gmail.com>
2018-07-18 19:44:39 +1200

    vARIOUS CHANGES

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -15,12 +15,52 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
 	private final static int TIMER_DELAY = 5;
 	
+	GameState gameState = GameState.Initialising;
+	
+	Ball ball;
+	Paddle paddle1, paddle2;
+	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
 		Timer timer = new Timer(TIMER_DELAY, this);
 		timer.start();
 	}
 	
+	public void createObjects() {
+		ball = new Ball(getWidth(), getHeight());
+		paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+		paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+	}
+	
+	private void update() {
+		switch(gameState) {
+			case Initialising: {
+				createObjects();
+				gameState = GameState.Playing;
+				break;
+			}
+			case Playing: {
+				break;
+			}
+			case GameOver: {
+				break;
+			}
+		}
+	}
+	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+			Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
+			g2d.setStroke(dashed);
+			g2d.setPaint(Color.WHITE);
+			g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+			g2d.dispose();
+	}
+	
+	private void paintSprite(Graphics g, Sprite sprite) {
+//		g.setColor(sprite.getColour());
+		g.fillRect(sprite.getXPosition(), sprite.getYPosition(),  sprite.getWidth(), sprite.getHeight());
+	}
 	
 	@Override
 	public void keyPressed(KeyEvent event) {
@@ -40,24 +80,17 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		
 	}
 	
-	private void paintDottedLine(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g.create();
-			Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
-			g2d.setStroke(dashed);
-			g2d.setPaint(Color.WHITE);
-			g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
-			g2d.dispose();
-	}
-	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		paintDottedLine(g);
+		if(gameState != GameState.Initialising) {
+			paintSprite(g, ball);
+			paintSprite(g, paddle1);
+			paintSprite(g, paddle2);
+		}
 	}
 	
-	private void update() {
-		
-	}
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
